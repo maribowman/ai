2741c79a959601d42e359fe4ab7d5b57ccd78aea
mari__ <bowman.marius@gmail.com>
2021-05-07 14:59:17 +0200

    implemented alpha-beta for custom player

--- Projects/3_Adversarial Search/my_custom_player.py
+++ Projects/3_Adversarial Search/my_custom_player.py
@@ -1,4 +1,3 @@
-
 from sample_players import DataPlayer
 
 
@@ -19,6 +18,7 @@ class CustomPlayer(DataPlayer):
       any pickleable object to the self.context attribute.
     **********************************************************************
     """
+
     def get_action(self, state):
         """ Employ an adversarial search technique to choose an action
         available in the current state calls self.queue.put(ACTION) at least
@@ -42,5 +42,71 @@ class CustomPlayer(DataPlayer):
         # EXAMPLE: choose a random move without any search--this function MUST
         #          call self.queue.put(ACTION) at least once before time expires
         #          (the timer is automatically managed for you)
+        #
         import random
-        self.queue.put(random.choice(state.actions()))
+
+        if state.ply_count < 4:
+            if self.data is not None and state in self.data:
+                self.queue.put(self.data[state])
+            else:
+                self.queue.put(random.choice(state.actions()))
+        else:
+            self.queue.put(self.alpha_beta_search(state=state, depth=5))
+
+    def alpha_beta_search(self, state, depth):
+        """ Return the move along a branch of the game tree that has the
+        best possible value.  A move is a pair of coordinates in (column, row)
+        order corresponding to a legal move for the searching player.
+        You can ignore the special case of calling this function from a terminal state.
+        """
+
+        def min_value(state, alpha, beta, depth):
+            """ Return the value for a win (+1) if the game is over,
+            otherwise return the minimum value over all legal child nodes.
+            """
+            if state.terminal_test():
+                return state.utility(self.player_id)
+            if depth <= 0:
+                return self.score(state)
+            min_val = float("inf")
+            for action in state.actions():
+                min_val = min(min_val, max_value(state.result(action), alpha, beta, depth - 1))
+                if min_val <= alpha:
+                    return min_val
+                beta = min(beta, min_val)
+            return min_val
+
+        def max_value(state, alpha, beta, depth):
+            """ Return the value for a loss (-1) if the game is over,
+            otherwise return the maximum value over all legal child nodes.
+            """
+            if state.terminal_test():
+                return state.utility(self.player_id)
+            if depth <= 0:
+                return self.score(state)
+            max_val = float("-inf")
+            for action in state.actions():
+                max_val = max(max_val, min_value(state.result(action), alpha, beta, depth - 1))
+                if max_val >= beta:
+                    return max_val
+                alpha = max(alpha, max_val)
+            return max_val
+
+        alpha = float("-inf")
+        beta = float("inf")
+        best_score = float("-inf")
+        best_move = None
+        for action in state.actions():
+            value = min_value(state.result(action), alpha, beta, depth - 1)
+            alpha = max(alpha, value)
+            if value > best_score:
+                best_score = value
+                best_move = action
+        return best_move
+
+    def score(self, state):
+        own_loc = state.locs[self.player_id]
+        opp_loc = state.locs[1 - self.player_id]
+        own_liberties = state.liberties(own_loc)
+        opp_liberties = state.liberties(opp_loc)
+        return len(own_liberties) - len(opp_liberties)
